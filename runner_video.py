3209295dc7d77dfef5c293720209dfd7ede47f2d
Richard Garrett <librichard11@gmail.com>
2024-10-01 22:07:11 -0400

    Removing extra file, more formatting

--- runner_video.py
+++ runner_video.py
@@ -14,7 +14,6 @@ class Player(pygame.sprite.Sprite):
 		self.image = self.player_walk[self.player_index]
 		self.rect = self.image.get_rect(midbottom = (80 ,300))
 		self.gravity = 0
-
 		self.jump_sound = pygame.mixer.Sound('audio/jump.mp3')
 		self.jump_sound.set_volume(0.5)
 
@@ -28,16 +27,13 @@ class Player(pygame.sprite.Sprite):
 	def apply_gravity(self):
 		self.gravity += 1
 		self.rect.y += self.gravity
-
 		if self.rect.bottom >= 300:	self.rect.bottom = 300
 
 	def animation_state(self):
 		if self.rect.bottom < 300:	self.image = self.player_jump
 		else:
 			self.player_index += 0.1
-			
-			if self.player_index >= len(self.player_walk): self.player_index = 0
-
+			if self.player_index >= len(self.player_walk):	self.player_index = 0
 			self.image = self.player_walk[int(self.player_index)]
 
 	def update(self):
@@ -52,15 +48,13 @@ class Obstacle(pygame.sprite.Sprite):
 		if type == 'fly':
 			fly_1 = pygame.image.load('graphics/fly/fly1.png').convert_alpha()
 			fly_2 = pygame.image.load('graphics/fly/fly2.png').convert_alpha()
-
 			self.frames = [fly_1 ,fly_2]
 			y_pos = 210
 		else:
 			snail_1 = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
 			snail_2 = pygame.image.load('graphics/snail/snail2.png').convert_alpha()
-			
 			self.frames = [snail_1 ,snail_2]
-			y_pos  = 300
+			y_pos = 300
 
 		self.animation_index = 0
 		self.image = self.frames[self.animation_index]
@@ -68,25 +62,22 @@ class Obstacle(pygame.sprite.Sprite):
 
 	def animation_state(self):
 		self.animation_index += 0.1 
-		
 		if self.animation_index >= len(self.frames):	self.animation_index = 0
-
 		self.image = self.frames[int(self.animation_index)]
+
+	def destroy(self):
+		if self.rect.x <= -100:	self.kill()
 
 	def update(self):
 		self.animation_state()
 		self.rect.x -= 6
 		self.destroy()
 
-	def destroy(self):
-		if self.rect.x <= -100:	self.kill()
-
 def display_score():
 	current_time = int(pygame.time.get_ticks() / 1000) - start_time
 	score_surf = test_font.render(f'Score: {current_time}' ,False ,(64 ,64 ,64))
 	score_rect = score_surf.get_rect(center = (400 ,50))
-
-	screen.blit(score_surf,score_rect)
+	screen.blit(score_surf ,score_rect)
 	
 	return current_time
 
@@ -113,7 +104,6 @@ def collisions(player ,obstacles):
 def collision_sprite():
 	if pygame.sprite.spritecollide(player.sprite ,obstacle_group ,False):
 		obstacle_group.empty()
-		
 		return False
 	else:	return True
 
@@ -129,16 +119,13 @@ def player_animation():
 		player_surf = player_walk[int(player_index)]
 
 pygame.init()
-
 screen = pygame.display.set_mode((800 ,400))
 pygame.display.set_caption('Runner')
-
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('font/Pixeltype.ttf' ,50)
 game_active = False
 start_time = 0
 score = 0
-
 bg_music = pygame.mixer.Sound('audio/music.wav')
 bg_music.play(loops = -1)
 
@@ -204,7 +191,7 @@ while True:
 		if event.type == pygame.QUIT:
 			pygame.quit()
 			exit()
-		
+
 		if game_active:
 			if event.type == pygame.MOUSEBUTTONDOWN:
 				if player_rect.collidepoint(event.pos) and player_rect.bottom >= 300:	player_gravity = -20
@@ -266,19 +253,18 @@ while True:
 		game_active = collision_sprite()
 		# game_active = collisions(player_rect,obstacle_rect_list)
 	else:
-		screen.fill((94,129,162))
-		screen.blit(player_stand,player_stand_rect)
+		screen.fill((94 ,129 ,162))
+		screen.blit(player_stand ,player_stand_rect)
 		obstacle_rect_list.clear()
 		player_rect.midbottom = (80 ,300)
 		player_gravity = 0
 
 		score_message = test_font.render(f'Your score: {score}' ,False ,(111 ,196 ,169))
 		score_message_rect = score_message.get_rect(center = (400 ,330))
-		screen.blit(game_name,game_name_rect)
+		screen.blit(game_name ,game_name_rect)
 
 		if score == 0:	screen.blit(game_message ,game_message_rect)
 		else:	screen.blit(score_message ,score_message_rect)
 
 	pygame.display.update()
 	clock.tick(60)
-	
